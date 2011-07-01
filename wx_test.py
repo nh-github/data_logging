c69a20f3f9b90bff383bd0569fec437e1523fba4
Noah Hafner <nmh+rcs@nomh.org>
2011-07-01 02:11:56 -1000

    added plot / figure to wxpython example
    mixing in matplotlib and checking the various examples

--- wx_test.py
+++ wx_test.py
@@ -2,11 +2,23 @@
 
 import collections
 import os
+import matplotlib
 import numpy
 import sys
 import time
+import wxversion
+wxversion.ensureMinimal('2.8')
+
+#some of this is from:
+#  /opt/local/share/py25-matplotlib/examples/user_interfaces/embedding_in_wx2.py
+matplotlib.use('WXAgg')
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+from matplotlib.figure import Figure
+
 import wx
 
+Q_FOR_DATA = collections.deque()
 
 class MyFrame(wx.Frame):
     """
@@ -71,7 +83,7 @@ class MyFrame(wx.Frame):
         panel.Layout()
 
         #store event data in here for now
-        self.datastore = collections.deque()
+        self.datastore = Q_FOR_DATA 
 
     def OnTimeToClose(self, evt):
         """Event handler for the button click."""
@@ -114,7 +126,7 @@ class MyFrame(wx.Frame):
         t=time.clock()
         btnLabel = self.FindWindowById(evt.GetId()).GetLabel()
         value = numpy.sin(numpy.pi*t)
-        print "button: %s, at T==%f, val==%f"%(btnLabel, t, value)
+        #print "button: %s, at T==%f, val==%f"%(btnLabel, t, value)
         (t, btnLabel)
         datum = {'time':t, 'channel':btnLabel, 'value':value}
         self.datastore.append(datum)
@@ -134,15 +146,133 @@ class MyFrame(wx.Frame):
         #print 'evt object',evt.GetEventObject()
         #print 'evt string',evt.GetString()
 
+class CanvasFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self,None,-1,
+                         'CanvasFrame',size=(550,350))
+
+        self.SetBackgroundColour(wx.NamedColor("WHITE"))
+        
+        self.figure = Figure()
+        self.axes = self.figure.add_subplot(111)
+        t = numpy.arange(0.0,3.0,0.01)
+        s = numpy.sin(2*numpy.pi*t)
+    
+        self.axes.plot(t,s)
+        self.canvas = FigureCanvas(self, -1, self.figure)
+
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
+        self.SetSizer(self.sizer)
+        self.Fit()
+
+        #self.add_toolbar()  # comment this out for no toolbar
+
+    def add_toolbar(self):
+        self.toolbar = NavigationToolbar2Wx(self.canvas)
+        self.toolbar.Realize()
+        if wx.Platform == '__WXMAC__':
+            # Mac platform (OSX 10.3, MacPython) does not seem to cope with
+            # having a toolbar in a sizer. This work-around gets the buttons
+            # back, but at the expense of having the toolbar at the top
+            self.SetToolBar(self.toolbar)
+        else:
+            # On Windows platform, default window size is incorrect, so set
+            # toolbar width to figure width.
+            tw, th = self.toolbar.GetSizeTuple()
+            fw, fh = self.canvas.GetSizeTuple()
+            # By adding toolbar in sizer, we are able to put it at the bottom
+            # of the frame - so appearance is closer to GTK version.
+            # As noted above, doesn't work for Mac.
+            self.toolbar.SetSize(wx.Size(fw, th))
+            self.sizer.Add(self.toolbar, 0, wx.LEFT | wx.EXPAND)
+        # update the axes menu on the toolbar
+        self.toolbar.update()
+
+    def OnPaint(self, event):
+        self.canvas.draw()
+
+TIMER_ID = wx.NewId()
+
+class PlotFigure(wx.Frame):
+
+    def __init__(self):
+        wx.Frame.__init__(self, None, -1, "Test embedded wxFigure")
+
+        self.fig = Figure((5,4), 75)
+        self.canvas = FigureCanvas(self, -1, self.fig)
+        self.toolbar = NavigationToolbar2Wx(self.canvas)
+        self.toolbar.Realize()
+
+        # On Windows, default frame size behaviour is incorrect
+        # you don't need this under Linux
+        tw, th = self.toolbar.GetSizeTuple()
+        fw, fh = self.canvas.GetSizeTuple()
+        self.toolbar.SetSize(Size(fw, th))
+
+        # Create a figure manager to manage things
+
+        # Now put all into a sizer
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        # This way of adding to sizer allows resizing
+        sizer.Add(self.canvas, 1, wx.LEFT|wx.TOP|wx.GROW)
+        # Best to allow the toolbar to resize!
+        sizer.Add(self.toolbar, 0, wx.GROW)
+        self.SetSizer(sizer)
+        self.Fit()
+        wx.EVT_TIMER(self, TIMER_ID, self.onTimer)
+
+    def init_plot_data(self):
+        # jdh you can add a subplot directly from the fig rather than
+        # the fig manager
+        a = self.fig.add_axes([0.075,0.1,0.75,0.85])
+        cax = self.fig.add_axes([0.85,0.1,0.075,0.85])
+        self.x = numpy.empty((120,120))
+        self.x.flat = numpy.arange(120.0)*2*numpy.pi/120.0
+        self.y = numpy.empty((120,120))
+        self.y.flat = numpy.arange(120.0)*2*numpy.pi/100.0
+        self.y = numpy.transpose(self.y)
+
+        self.im = a.imshow( z, cmap=cm.jet)#, interpolation='nearest')
+        self.fig.colorbar(self.im,cax=cax,orientation='vertical')
+
+    def GetToolBar(self):
+        # You will need to override GetToolBar if you are using an
+        # unmanaged toolbar in your frame
+        return self.toolbar
+
+    def onTimer(self, evt): 
+        self.x += numpy.pi/15
+        self.y += numpy.pi/20
+        z = numpy.sin(self.x) + numpy.cos(self.y)
+        self.im.set_array(z)
+        self.canvas.draw()
+        #self.canvas.gui_repaint()  # jdh wxagg_draw calls this already
+
+    def onEraseBackground(self, evt):
+        # this is supposed to prevent redraw flicker on some X servers...
+        pass
+
 
 class MyApp(wx.App):
     def OnInit(self):
-        frame = MyFrame(None, "Simple wxPython App")
-        self.SetTopWindow(frame)
+        #frame1 = MyFrame(None, "Simple wxPython App")
+        #self.SetTopWindow(frame1)
+        #frame1.Show(True)
 
-        print "Print statements go to this stdout window by default."
+        #frame2 = CanvasFrame()
+        #frame2.Show(True)
 
-        frame.Show(True)
+        frame3 = PlotFigure()
+        frame3.init_plot_data()
+
+        # Initialise the timer - wxPython requires this to be connected to
+        # the receiving event handler
+        t = Timer(frame3, TIMER_ID)
+        t.Start(200)
+        frame3.Show(True)
+
+        
         return True
         
 app = MyApp(redirect=True)
