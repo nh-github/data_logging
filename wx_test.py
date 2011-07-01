0959990900ff51bde44e78719c8e2b9f25a7aa2a
Noah Hafner <nmh+rcs@nomh.org>
2011-07-01 02:10:01 -1000

    expanded wxpython test a bit, also events
    added some more buttons, exploring event handlers and the like
    a small bit of datastructure testing, but dropped

--- wx_test.py
+++ wx_test.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env pythonw
 
+import collections
+import os
+import numpy
+import sys
+import time
 import wx
 
 
@@ -10,7 +15,7 @@ class MyFrame(wx.Frame):
     """
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, -1, title,
-                          pos=(150, 150), size=(350, 200))
+                          pos=(150, 150), size=(350, 300))
 
         # Create the menubar
         menuBar = wx.MenuBar()
@@ -37,34 +42,97 @@ class MyFrame(wx.Frame):
         panel = wx.Panel(self)
 
         # and a few controls
-        text = wx.StaticText(panel, -1, "Hello World!")
+        text = wx.StaticText(panel, -1, "Event testing")
         text.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
         text.SetSize(text.GetBestSize())
         btn = wx.Button(panel, -1, "Close")
-        funbtn = wx.Button(panel, -1, "Just for fun...")
+        self.btn1 = wx.Button(panel, -1, "1")
+        self.btn2 = wx.Button(panel, -1, "2")
+        self.btn3 = wx.Button(panel, -1, "3")
+        #self.btn4 = wx.Button(panel, -1, "4")
 
         # bind the button events to handlers
         self.Bind(wx.EVT_BUTTON, self.OnTimeToClose, btn)
-        self.Bind(wx.EVT_BUTTON, self.OnFunButton, funbtn)
+        self.Bind(wx.EVT_BUTTON, self.OnFunButton, self.btn1)
+        self.Bind(wx.EVT_BUTTON, self.OnFunButton, self.btn2)
+        self.Bind(wx.EVT_BUTTON, self.OnFunButton, self.btn3)
+        #self.Bind(wx.EVT_BUTTON, self.OnFunButton, self.btn4)
 
         # Use a sizer to layout the controls, stacked vertically and with
         # a 10 pixel border around each
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(text, 0, wx.ALL, 10)
+        sizer.Add(self.btn1, 0, wx.ALL, 10)
+        sizer.Add(self.btn2, 0, wx.ALL, 10)
+        sizer.Add(self.btn3, 0, wx.ALL, 10)
         sizer.Add(btn, 0, wx.ALL, 10)
-        sizer.Add(funbtn, 0, wx.ALL, 10)
+        #sizer.Add(self.btn4, 0, wx.ALL, 10)
         panel.SetSizer(sizer)
         panel.Layout()
 
+        #store event data in here for now
+        self.datastore = collections.deque()
 
     def OnTimeToClose(self, evt):
         """Event handler for the button click."""
-        print "See ya later!"
+        outfile = open('testfile.csv','w')
+        #note: this is horrible, not just bad, it is horribly bad
+        axes = {}
+        for datum in self.datastore:
+            t = datum['time']
+            c = datum['channel']
+            v = datum['value']
+            if c not in axes:
+                axes[c] = []
+            axes[c].append((c,t,v))
+        chans = axes.keys()
+        chans.sort()
+        chanLengths = []
+        for chan in chans:
+            chanLengths.append(len(axes[chan]))
+        maxChLen = max(chanLengths)
+        for chan in chans:
+            outfile.write("channel,time,value,")
+        outfile.write("\n")
+        #for i in range(len(axes.items()[0][1])):
+        for i in range(maxChLen):
+            for chan in chans:
+                try:
+                    c = axes[chan][i][0]
+                    t = axes[chan][i][1]
+                    v = axes[chan][i][2]
+                    datum = (c,t,v)
+                except:
+                    datum = ('None',0,0)
+                outfile.write("%s,%.3f,%.3f,"%datum)
+            outfile.write("\n")
+
         self.Close()
 
     def OnFunButton(self, evt):
         """Event handler for the button click."""
-        print "Having fun yet?"
+        t=time.clock()
+        btnLabel = self.FindWindowById(evt.GetId()).GetLabel()
+        value = numpy.sin(numpy.pi*t)
+        print "button: %s, at T==%f, val==%f"%(btnLabel, t, value)
+        (t, btnLabel)
+        datum = {'time':t, 'channel':btnLabel, 'value':value}
+        self.datastore.append(datum)
+        #print dir(evt)
+        #print dir(self.btn4)
+        #print 'handle',self.btn4.GetHandle()
+        #print 'id',self.btn4.GetId()
+        #print 'label',self.btn4.GetLabel()
+        #print 'label text',self.btn4.GetLabelText()
+        #print 'name',self.btn4.GetName()
+        #print ''
+        #print 'evt id',evt.GetId()
+        #print '?', self.FindWindowById(evt.GetId())
+        #print 'evt timestamp',evt.GetTimestamp()
+        #print 'evt data',evt.GetClientData()
+        #print 'evt object',evt.GetClientObject()
+        #print 'evt object',evt.GetEventObject()
+        #print 'evt string',evt.GetString()
 
 
 class MyApp(wx.App):
