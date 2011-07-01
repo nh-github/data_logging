9a924e1aeefe8bf85f22d7a9fc9373a9dca5561b
Noah Hafner <nmh+rcs@nomh.org>
2011-07-01 02:05:29 -1000

    initial commit
    working on small python program to acquire data from a phidgets
    accelerometer, plot the data in near-realtime, and optionally record the
    data to disk.
    
    this first commit starts with wxpython

---
+++ wx_test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+#how can we import wx or wxPython.wx as wxpy or whatever
+from wxPython.wx import *
+
+ID_ABOUT = 101
+ID_EXIT  = 102
+
+class MyFrame(wxFrame):
+    def __init__(self, parent, ID, title):
+        wxFrame.__init__(self, parent, ID, title,
+                         wxDefaultPosition, wxSize(200, 150))
+        self.CreateStatusBar()
+        self.SetStatusText("This is the statusbar")
+
+        menu = wxMenu()
+        menu.Append(ID_ABOUT, "&About",
+                    "More information about this program")
+        menu.AppendSeparator()
+        menu.Append(ID_EXIT, "E&xit", "Terminate the program")
+
+        menuBar = wxMenuBar()
+        menuBar.Append(menu, "&File");
+
+        self.SetMenuBar(menuBar)
+
+        EVT_MENU(self, ID_ABOUT, self.OnAbout)
+        EVT_MENU(self, ID_EXIT,  self.TimeToQuit)
+
+    def OnAbout(self, event):
+        dlg = wxMessageDialog(self, "This sample program shows off\n"
+                              "frames, menus, statusbars, and this\n"
+                              "message dialog.",
+                              "About Me", wxOK | wxICON_INFORMATION)
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def TimeToQuit(self, event):
+        self.Close(true)
+
+class MyApp(wxApp):
+    def OnInit(self):
+        frame = MyFrame(NULL, -1, "Hello from wxPython")
+        frame.Show(true)
+        self.SetTopWindow(frame)
+        return true
+
+app = MyApp(0)
+app.MainLoop()
